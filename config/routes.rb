cdd435acd80138d09416009356a48cd2c84040fd
Amol Rockon <rails@kunalinfotech.net>
2012-01-24 15:00:27 +0530

    changes member

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ IIAArchitechture::Application.routes.draw do
   
   devise_for :admins, :path_names => { :sign_in =>"login", :sign_out =>"logout"}
   
-   resources :committees    
+   resources :committees  
+   resources :members  
   
   
    namespace :admin do 
