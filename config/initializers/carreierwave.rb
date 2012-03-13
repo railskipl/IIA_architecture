56441cbcfe768b5a80ae7cb5300ff9f4d2a3ca89
Anup <rails@kunalinfotech.net>
2012-03-13 15:34:33 +0530

    minor changes

--- config/initializers/carreierwave.rb
+++ config/initializers/carreierwave.rb
@@ -15,7 +15,5 @@ CarrierWave.configure do |config|
    config.fog_public     = true                                   # optional, defaults to true
    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
-
-
   
 end
