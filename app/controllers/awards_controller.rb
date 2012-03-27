806c8471d9d72ce7759125a464d1c7a875cea914
Anup <rails@kunalinfotech.net>
2012-03-27 17:50:33 +0530

    award edit issue solved

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,7 +1,7 @@
 class AwardsController < ApplicationController
   
   def index
-    @awards = Award.find(:all).paginate(:per_page => 9, :page => params[:page])
+    @awards = Award.find(:all, :order => 'id asc').paginate(:per_page => 9, :page => params[:page])
   end
   
   def show
