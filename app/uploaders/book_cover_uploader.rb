6abe49d6390ba77fa215a7bb0c81ff8730e86124
Anup <rails@kunalinfotech.net>
2012-04-04 16:20:30 +0530

    uploader updates

--- app/uploaders/book_cover_uploader.rb
+++ app/uploaders/book_cover_uploader.rb
@@ -7,15 +7,19 @@ class BookCoverUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+	
+	def cache_dir
+      "#{Rails.root}/tmp/uploads"
+  	end
+	
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
