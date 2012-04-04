6c6c953290703c8b19e147be8c23772bdf77c777
Anup <rails@kunalinfotech.net>
2012-04-04 18:14:49 +0530

    libray updates

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -2,7 +2,7 @@ class LibrariesController < ApplicationController
 	layout 'news'
 
 	def index
-		@libraries = Library.find(:all, :order => 'created_at asc').paginate(:per_page => 5, :page => params[:page])
+		@libraries = Library.find(:all, :order => 'created_at asc').paginate(:per_page => 9, :page => params[:page])
 	end
 	
 	def show
