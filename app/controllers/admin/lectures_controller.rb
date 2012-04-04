0804ee3cbcdf302a45513b941cd63105f03c7178
Anup <rails@kunalinfotech.net>
2012-04-04 18:08:42 +0530

    libray updates

--- app/controllers/admin/lectures_controller.rb
+++ app/controllers/admin/lectures_controller.rb
@@ -1,41 +1,50 @@
 class Admin::LecturesController <  Admin::BaseController
 	
 	def index
-		@lectures = Lecture.find(:all)
-	end
+     @lectures = Lecture.find(:all)
+  end
+  
+  def new
+    @lecture = Lecture.new
+  end
+  
+  def create
+     @lecture = Lecture.new(params[:lecture])
+     if @lecture.save
+       flash[:notice] = "Lecture created successfully"
+       redirect_to admin_lectures_path
+     else
+       render :new
+     end
+   end
+   
+   def show
+     @lecture = Lecture.find(params[:id])
+   end
+
+   def edit
+     @lecture = Lecture.find(params[:id])
+   end
+
+   def update
+     @lecture = Lecture.find(params[:id])
+     if @lecture.update_attributes(params[:lecture])
+       flash[:notice] = "Lecture saved successfully"
+       redirect_to admin_lectures_path
+     else
+       render :new
+     end
+   end
+
+
+   def destroy
+     @lecture = Lecture.find(params[:id])
+
+     if @lecture.destroy
+       flash[:notice] = "Lecture deleted successfully"
+       redirect_to admin_lectures_path
+     end
+
+   end
 	
-	def new
-		@lectures = Lecture.new
-	end
-	
-	def create
-		@lecture = Lecture.new(params[:lecture])
-		if @lecture.save
-			flash[:notce] = "Lecture is successfully saved"
-		else
-			render :new
-		end
-	end
-	
-	def edit
-		@lecture = Lecture.find(params[:id])
-	end
-	
-	def update
-		@lecture = Lecture.find(params[:id])
-		if @lecture.update_attributes(params[:lecture])
-			flash[:notice] = "Lecture updated successfully"
-			redirect_to admin_lectures_path
-		else
-			render :new
-		end
-	end
-	
-	def destroy
-		@lecture = Lecture.find(params[:id])
-		if @lecture.destroy
-			flash[:notice] = "Lecture has been destroyed successfully"
-			redirect_to admin_lectures_path
-		end
-	end 
 end
