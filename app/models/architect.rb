ab18969f89f4012a45e18945ec4b1f38c308340d
Your Name <rails@kunalinfotech.net>
2012-04-10 11:32:13 +0530

    Architect updates

--- app/models/architect.rb
+++ app/models/architect.rb
@@ -1,5 +1,5 @@
 class Architect < ActiveRecord::Base
-	attr_accessible :title
+	attr_accessible :title, :description
 	has_many :galleries, :dependent => :destroy
 	accepts_nested_attributes_for :galleries
 end
