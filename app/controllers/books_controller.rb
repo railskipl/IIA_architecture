18ee5a5cef511b8d182c72385c037b47e24ca729
Your Name <rails@kunalinfotech.net>
2012-04-12 17:47:05 +0530

    pagination added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
 	layout 'members'
 	
 	def index
-  		@books = Book.find(:all)
+  		@books = Book.find(:all).paginate(:per_page => 2, :page => params[:page])
   		
   	end
   	def show
